b210d82ea61df02c0221d1b7b029e62ebcb70113
dcweinstein <davidw1@iastate.edu>
2015-04-19 21:25:16 -0500

    added connectivity to DB for candidates. Inserted temp side bar picture

--- app/scripts/controllers/candidates.js
+++ app/scripts/controllers/candidates.js
@@ -8,11 +8,9 @@
  * Controller of the webdevApp
  */
 angular.module('webdevApp')
-  .controller('CandidatesCtrl', function ($scope) {
-    $scope.candidates = [
-      {name: 'Hilary Clinton', party: 'Democrat', position: 'Former Secretary of State', img: 'images/HC.png', declaredDate: '04/12/2015'},
-      {name: 'Ted Cruz', party: 'Republican', position: 'Texas Senator', img: 'images/TC.jpg', declaredDate: '03/23/2015'},
-      {name: 'Rand Paul', party: 'Republican', position: 'Kentucky Senator', img: 'images/RP.jpg', declaredDate: '04/07/2015'},
-      {name: 'Marco Rubio', party: 'Republican', position: 'Florida Senator', img: 'images/MR.jpg', declaredDate: '04/13/2015'}
-    ];
+  .controller('CandidatesCtrl', function ($scope, $http) {
+    $http.get('http://localhost:8080/api/candidates').
+        success(function(data) {
+            $scope.candidates = data;
+        });
   });
